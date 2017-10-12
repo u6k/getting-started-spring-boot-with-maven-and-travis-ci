1a84afb42b5f1909ee9bee214f0cc7febf683bff
u6k <u6k.apps@gmail.com>
2017-10-12 13:46:47 +0900

    Spring Initializrで生成したSpring Bootプロジェクトを格納しました。 #3

---
+++ src/main/java/me/u6k/template/myapp/MyAppApplication.java
@@ -0,0 +1,12 @@
+package me.u6k.template.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppApplication.class, args);
+	}
+}
